44d157d0f90ab2b81b266774122c3f41517ee034
louiscklaw <louiscklaw@users.noreply.github.com>
2023-04-30 13:30:04 +0800

    update,

--- express-src/routes/meetingRequest.js
+++ express-src/routes/meetingRequest.js
@@ -3,13 +3,14 @@ var router = express.Router();
 const TelegramBot = require('node-telegram-bot-api');
 
 let TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
-let HELLOWORLD_GROUP_CHATID = '-986776285';
+let TELEGRAM_CHATID = process.env.TELEGRAM_CHATID;
+
 const bot = new TelegramBot(TELEGRAM_BOT_TOKEN, { polling: false });
 
 function sendTelegramMessage({ roomId }) {
   console.log('sendTelegramMessage');
   console.log({ roomId });
-  bot.sendMessage(HELLOWORLD_GROUP_CHATID, `//localhost:8080/${roomId}`);
+  bot.sendMessage(TELEGRAM_CHATID, `//localhost:8080/${roomId}`);
 }
 
 // http://localhost:3000/meetingRequest/r/helloworld_room
