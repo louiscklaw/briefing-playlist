5d9e9656c898fa6a073a8186b94f740892ed7eb4
louiscklaw <louiscklaw@users.noreply.github.com>
2023-05-02 14:43:59 +0800

    update,

---
+++ meet.louislabs.com/test/public/test_meeting_request_from_portfolio.js
@@ -0,0 +1,33 @@
+
+async function postData(url = "", data = {}) {
+  // Default options are marked with *
+  const response = await fetch(url, {
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    // mode: "cors", // no-cors, *cors, same-origin
+    // cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    // credentials: "same-origin", // include, *same-origin, omit
+    headers: {
+      "Content-Type": "application/json",
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: "follow", // manual, *follow, error
+    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(data), // body data type must match "Content-Type" header
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
+}
+
+document.addEventListener("DOMContentLoaded", ()=>{
+  document.querySelector('#test_meeting_request_from_portfolio').addEventListener('click', () =>{
+    postData("//localhost:3000/meetingRequest/r/req_from_portfolio",
+    { comments:
+`
+incomint meeting request from portfolio
+🍊
+`.trim()
+    }
+    ).then((data) => {
+    console.log(data); // JSON data parsed by `data.json()` call
+  });
+  })
+})
